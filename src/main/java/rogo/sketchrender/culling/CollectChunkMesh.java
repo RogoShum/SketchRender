6610176607c218e481182abb5088b33478001cad
RogoShum <g1269314290@gmail.com>
2024-12-30 02:38:11 +0800

    1

---
+++ src/main/java/rogo/sketchrender/culling/CollectChunkMesh.java
@@ -0,0 +1,42 @@
+package rogo.sketchrender.culling;
+
+import me.jellysquid.mods.sodium.client.gl.util.VertexRange;
+import me.jellysquid.mods.sodium.client.model.quad.properties.ModelQuadFacing;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildContext;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildOutput;
+import me.jellysquid.mods.sodium.client.render.chunk.data.SectionRenderDataUnsafe;
+import me.jellysquid.mods.sodium.client.util.task.CancellationToken;
+
+public class CollectChunkMesh {
+    public static void update(ChunkBuildContext buildContext, CancellationToken cancellationToken, ChunkBuildOutput chunkBuildOutput) {
+        if (this.allocations[localSectionIndex] != null) {
+            this.allocations[localSectionIndex].delete();
+            this.allocations[localSectionIndex] = null;
+        }
+
+        this.allocations[localSectionIndex] = allocation;
+        long pMeshData = this.getDataPointer(localSectionIndex);
+        int sliceMask = 0;
+        int vertexOffset = allocation.getOffset();
+
+        for(int facingIndex = 0; facingIndex < ModelQuadFacing.COUNT; ++facingIndex) {
+            VertexRange vertexRange = ranges[facingIndex];
+            int vertexCount;
+            if (vertexRange != null) {
+                vertexCount = vertexRange.vertexCount();
+            } else {
+                vertexCount = 0;
+            }
+
+            SectionRenderDataUnsafe.setVertexOffset(pMeshData, facingIndex, vertexOffset);
+            SectionRenderDataUnsafe.setElementCount(pMeshData, facingIndex, (vertexCount >> 2) * 6);
+            if (vertexCount > 0) {
+                sliceMask |= 1 << facingIndex;
+            }
+
+            vertexOffset += vertexCount;
+        }
+
+        SectionRenderDataUnsafe.setSliceMask(pMeshData, sliceMask);
+    }
+}
---
+++ src/main/java/rogo/sketchrender/mixin/sodium/MixinChunkBuilderMeshingTask.java
@@ -0,0 +1,22 @@
+package rogo.sketchrender.mixin.sodium;
+
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildContext;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildOutput;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.tasks.ChunkBuilderMeshingTask;
+import me.jellysquid.mods.sodium.client.util.task.CancellationToken;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ChunkBuilderMeshingTask.class, remap = false)
+public class MixinChunkBuilderMeshingTask {
+
+    @Inject(method = "execute(Lme/jellysquid/mods/sodium/client/render/chunk/compile/ChunkBuildContext;Lme/jellysquid/mods/sodium/client/util/task/CancellationToken;)Lme/jellysquid/mods/sodium/client/render/chunk/compile/ChunkBuildOutput;", at = @At("RETURN"))
+    private void repackageResults(ChunkBuildContext buildContext, CancellationToken cancellationToken, CallbackInfoReturnable<ChunkBuildOutput> cir) {
+        ChunkBuildOutput result = cir.getReturnValue();
+        if (result != null) {
+
+        }
+    }
+}
